aafa74a3923e9dfa0d410721d34eef779df5d2a0
Milan Jurhan <m.jurhan@eslgaming.com>
2017-06-05 18:49:39 +0200

    add example config

---
+++ sshConfig.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+    gitName: 'Your Name',
+    gitEmail: 'your.email@provider.com',
+    sshPrivateKeyPath: '../../.ssh/id_rsa',
+    sshPublicKeyPath: '../../.ssh/id_rsa.pub'
+}
