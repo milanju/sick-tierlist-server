cfc0bc697310cd0db6fed7188d14e97c92936742
Milan Jurhan <m.jurhan@eslgaming.com>
2017-06-05 18:58:51 +0200

    add documentation

--- fetchStrategies/jsonHttpStrategy/jsonHttpStrategy.js
+++ fetchStrategies/jsonHttpStrategy/jsonHttpStrategy.js
@@ -1,5 +1,12 @@
 const request = require('request-promise-native');
 
+/**
+ * @param {string} url Url to fetch the tierlist JSON from.
+ * @param {function} parser Function that gets the plain tierlist object and returns
+ * a valid tierlist object.
+ * 
+ * @returns {Promise<Tierlist>} Promise containing the valid plain tierlist object.
+ */
 function jsonHttpStrategy(url, parser) {
     return request(url).then(body => {
         return parser(JSON.parse(body));
